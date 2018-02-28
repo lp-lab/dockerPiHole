507438d4a5e690419db9ffe176e8608fb3a7d844
Luca 'meti' P <meti-@users.noreply.github.com>
2018-02-28 11:50:10 +0100

    Updated pi-hole to v3.3

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export USER=pihole
-export CORE_TAG=v3.2.1
+export CORE_TAG=v3.3
 
 wget https://raw.githubusercontent.com/pi-hole/pi-hole/${CORE_TAG}/automated%20install/basic-install.sh
 
@@ -25,8 +25,18 @@ EOF
 #/bin/sed -i '1405s/.*/installPihole  | tee ${tmpLog}/g' basic-install.sh
 /bin/sed -i 's/updatePihole | tee ${tmpLog}/installPihole  | tee ${tmpLog}/g' basic-install.sh
 /bin/sed -i 's|debconf-apt-progress -- "\${PKG_INSTALL\[@\]}" "\${installArray\[@\]}"|"\${PKG_INSTALL\[@\]}" "\${installArray\[@\]}"|g' basic-install.sh
+groupadd pihole
+useradd -r -s /usr/sbin/nologin -g pihole pihole
+mkdir /var/run/pihole
+chown pihole:pihole /var/run/pihole
 /bin/bash basic-install.sh --unattended
 /bin/rm basic-install.sh
+chown www-data:www-data /var/www/html
+chmod 775 /var/www/html
+# Give pihole access to the Web server group
+usermod -a -G www-data pihole
+# enable fastcgi and fastcgi-php
+lighty-enable-mod fastcgi fastcgi-php > /dev/null || true
 
 apt-get -y install php7.0-zip
 apt-get clean
